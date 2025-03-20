df6a27deb5c82825c760fe72a91b1f17afaae588
Mauricio Torti <mauriciotorti1@gmail.com>
2025-03-20 12:18:37 -0300

    Agregado de Mapas con zoom dinamico y marcadores

--- src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,9 +1,96 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, effect, ElementRef, signal, viewChild } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import mapboxgl from 'mapbox-gl';
+import { DecimalPipe, JsonPipe } from '@angular/common';
+
+
+mapboxgl.accessToken = environment.mapboxKey;
+
+
+
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [
+    DecimalPipe,
+    JsonPipe
+  ],
   templateUrl: './fullscreen-map-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: `
+    div {
+      width: 100vw;
+      height: calc( 100vh - 64px);
+    }
+
+    #controls {
+      background-color: white;
+      padding: 10px;
+      border-radius: 5px;
+      position: fixed;
+      bottom: 25px;
+      right: 20px;
+      z-index: 9999;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+      border: 1px solid #e2e8f0;
+      width: 250px;
+    }
+
+  `,
 })
-export default class FullscreenMapPageComponent { }
+export default class FullscreenMapPageComponent implements AfterViewInit {
+
+  divElement = viewChild<ElementRef>('map')
+
+  zoom = signal(14);
+
+  coordinates = signal({ lat: 40, lng: -74.5 });
+
+
+
+  map = signal<mapboxgl.Map|null>(null);
+
+  zoomEffect = effect(() => {
+    if(!this.map()) return;
+
+    this.map()?.setZoom(this.zoom());
+  })
+
+  async ngAfterViewInit() {
+    if (!this.divElement()) return;
+
+    const element = this.divElement()?.nativeElement;
+
+    const {lat , lng} = this.coordinates();
+
+    await new Promise((resolve) => setTimeout(resolve, 80))
+
+    const map = new mapboxgl.Map({
+      container: element, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [lng, lat], // starting position [lng, lat]
+      zoom: this.zoom(), // starting zoom
+    });
+
+    this.mapListener(map);
+  }
+
+  mapListener(map : mapboxgl.Map) {
+
+    map.on('zoomend', (event) => {
+      const newZoom = event.target.getZoom();
+      this.zoom.set(newZoom);
+    })
+
+    map.on('moveend', (event) => {
+      const center = event.target.getCenter();
+      this.coordinates.set(center);
+    })
+
+    map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.NavigationControl());
+    map.addControl(new mapboxgl.ScaleControl());
+
+    this.map.set(map);
+  }
+ }
--- src/app/pages/markets-page/markers-page.component.ts
+++ src/app/pages/markets-page/markers-page.component.ts
@@ -1,15 +1,81 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { routes } from '../../app.routes';
-import { filter, map } from 'rxjs';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, signal, viewChild } from '@angular/core';
+import mapboxgl, { MapMouseEvent } from 'mapbox-gl';
+import { environment } from '../../../environments/environment';
+import {v4} from 'uuid'
 
+
+interface Marker {
+  id: string;
+  mapboxMarker : mapboxgl.Marker;
+}
+
+
+mapboxgl.accessToken = environment.mapboxKey;
 @Component({
   selector: 'app-markets-page',
   imports: [],
   templateUrl: './markers-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class MarkersPageComponent {
+export default class MarkersPageComponent implements AfterViewInit{
+  divElement = viewChild<ElementRef>('map')
+  map = signal<mapboxgl.Map|null>(null);
+
+  markers = signal<Marker[]>([])
+
+
+
+  async ngAfterViewInit(){
+    if (!this.divElement()) return;
+
+  const element = this.divElement()?.nativeElement;
+
+  await new Promise((resolve) => setTimeout(resolve, 80))
+
+  const map = new mapboxgl.Map({
+    container: element, // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-122.40985, 37.793085], // starting position [lng, lat]
+    zoom: 14,});
+
+
+    // const marker =new  mapboxgl.Marker({
+    //   draggable : true
+    // })
+    //   .setLngLat([-122.40985, 37.793085])
+    //   .addTo(map)
+
+    this.mapListener(map)
+  }
+
+  mapListener(map : mapboxgl.Map){
+    map.on('click', (event) => this.mapClick(event));
+
+
+    this.map.set(map)
+  }
+
+  mapClick(event : MapMouseEvent){
+    if (!this.map()) return;
+
+    const map = this.map()!;
+    const coords = event.lngLat;
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const mapBoxMarker = new mapboxgl.Marker({
+      color : color
+    }).setLngLat(coords)
+      .addTo(map)
+
+    const newMarker : Marker = {
+      id : v4(),
+      mapboxMarker : mapBoxMarker
+    }
+
+    this.markers.set([newMarker, ...this.markers()])
+  }
 
 
 }
